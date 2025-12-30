f52c8e5b96068e80b7822bdd7ddc685e21779ea8
bayankh3421-stack <bayankh3421@gmail.com>
2025-12-30 09:10:55 +0300

    Update database connection parameters

--- config/db.php
+++ config/db.php
@@ -1,19 +1,13 @@
 <?php
 
-$host = "aws-0-us-east-1.pooler.supabase.com";
-$port = "5432";
-$dbname = "postgres";
-$user = "postgres.bxcnoiolbrxulbqgozpm";
-$pass = "Bb?0792017997";
-
-$conn = pg_connect("
-    host=$host
-    port=$port
-    dbname=$dbname
-    user=$user
-    password=$pass
-    sslmode=require
-");
+$conn = pg_connect(
+    "host=aws-0-us-east-1.pooler.supabase.com
+     port=6543
+     dbname=postgres
+     user=postgres.bxcnoiolbrxulbqgozpm
+     password=Bb?0792017997
+     sslmode=require"
+);
 
 if (!$conn) {
     die("فشل الاتصال بقاعدة البيانات");
