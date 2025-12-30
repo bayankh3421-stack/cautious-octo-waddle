da6fcd25cf5dcd9469fa89a65fe99083de697de0
bayankh3421-stack <bayankh3421@gmail.com>
2025-12-30 09:23:17 +0300

    Update database connection details in db.php

--- config/db.php
+++ config/db.php
@@ -1,10 +1,10 @@
 <?php
 
 $conn = pg_connect(
-    "host=db.bxcnoiolbrxulbqgozpm.supabase.co
-     port=5432
+    "host=aws-0-us-east-1.pooler.supabase.com
+     port=6543
      dbname=postgres
-     user=postgres
+     user=postgres.bxcnoiolbrxulbqgozpm
      password=Bb?0792017997
      sslmode=require"
 );
